e53a548d28945a859ab7a4865f6f7dfbca131d2f
zlisheng <zlisheng@paypal.com>
2014-12-11 21:27:39 +0800

    update test_control_flow.js
    updated test_control_flow.js to make more complex cases

--- test/test_control_flow.js
+++ test/test_control_flow.js
@@ -4,25 +4,36 @@ describe('test control flow', function() {
   });
   
   it('flow 1', function() {
-	element(by.model('first')).sendKeys(4);
+	element(by.model('first')).clear().sendKeys(1);
 	console.log("flow 1::1");
+    element(by.model('second')).clear().sendKeys(2);
 	console.log("flow 1::2");
+	element(by.id('gobutton')).click();
 	console.log("flow 1::3");
-    element(by.model('second')).sendKeys(5);
+	expect(element(by.binding('latest')).getText()).toEqual('3');
 	console.log("flow 1::4");
   });
   
   it('flow 2', function() {
+	element(by.model('first')).clear().sendKeys(3);
 	console.log("flow 2::a");
-	element(by.id('gobutton')).click();
+    element(by.model('second')).clear().sendKeys(4);
 	console.log("flow 2::b");
+	element(by.id('gobutton')).click();
 	console.log("flow 2::c");
+	expect(element(by.binding('latest')).getText()).toEqual('7');
 	console.log("flow 2::d");
   });
   
   it('flow 3', function(){
-	expect(element(by.binding('latest')).getText()).
-        toEqual('9');
+	element(by.model('first')).clear().sendKeys(5);
+	console.log("flow 3::A");
+    element(by.model('second')).clear().sendKeys(6);
+	console.log("flow 3::B");
+	element(by.id('gobutton')).click();
+	console.log("flow 3::C");
+	expect(element(by.binding('latest')).getText()).toEqual('11');
+	console.log("flow 3::D");
   });
   
 });
